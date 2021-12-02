a9be20ce29993410bf195bd00fc8c79cf2327749
VojtaSara <46105170+VojtaSara@users.noreply.github.com>
2021-12-02 15:54:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('b1').addEventListener('click', function() {
   document.getElementById('video').innerHTML =
-  '<iframe class="responsive-iframe" src="https://www.youtube.com/embed/KDqzKqhbmKg-A?rel=0&autoplay=1" width="1280" height="720" frameborder="0" allowfullscreen="false">';
+  '<iframe class="responsive-iframe" src="https://www.youtube.com/embed/KDqzKqhbmKg?rel=0&autoplay=1" width="1280" height="720" frameborder="0" allowfullscreen="false">';
   });
 
 document.getElementById('b2').addEventListener('click', function() {
