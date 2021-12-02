4e316c5fec155ba6d1d9f2862e98de75357114e8
VojtaSara <46105170+VojtaSara@users.noreply.github.com>
2021-12-02 15:24:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById('b1').addEventListener('click', function() {
+  document.getElementById('video').innerHTML =
+  '<iframe class="responsive-iframe" src="https://www.youtube.com/embed/KDqzKqhbmKg-A?rel=0&autoplay=1" width="1280" height="720" frameborder="0" allowfullscreen="false">';
+  });
+
+document.getElementById('b2').addEventListener('click', function() {
+  document.getElementById('video').innerHTML =
+  '<iframe class="responsive-iframe" src="https://www.youtube.com/embed/LURUnSw8oDM?rel=0&autoplay=1" width="1280" height="720" frameborder="0" allowfullscreen="false">';
+  });
+
+document.getElementById('b3').addEventListener('click', function() {
+  document.getElementById('video').innerHTML =
+  '<iframe class="responsive-iframe" src="https://www.youtube.com/embed/r8dn75zVE6c?rel=0&autoplay=1" width="1280" height="720" frameborder="0" allowfullscreen="false">';
+  });
+
+document.getElementById('b4').addEventListener('click', function() {
+  document.getElementById('video').innerHTML = 
+  '<iframe class="responsive-iframe" src="https://www.youtube.com/embed/uCLb1wept-A?rel=0&autoplay=1" width="1280" height="720" frameborder="0" allowfullscreen="false">';
+  });
